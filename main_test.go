85accf76b4a7189f82effc657ce8db85c6e2eaec
Marcos Medrano <marcosmedrano0@gmail.com>
2022-12-08 12:27:29 -0300

    Added tests to check for status codes

--- main_test.go
+++ main_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 )
 
+// Checks if client receives body and headers sent from origin
 func TestHandleSimpleRequest(t *testing.T) {
 	const expectedBodyStr string = "Hello from the other side"
 	const customHeaderKey string = "X-Test-Header"
@@ -38,6 +39,40 @@ func TestHandleSimpleRequest(t *testing.T) {
 
 	if v := res.Header.Get(customHeaderKey); v != customHeaderVal {
 		t.Errorf("Expected header value '%v', got '%v'", customHeaderVal, v)
+	}
+}
+
+// Checks if client receives HTTP status code sent by origin
+func TestHandleTeapotRequest(t *testing.T) {
+	// Mock objects
+	req := httptest.NewRequest(http.MethodGet, "/teapot", nil)
+	rec := httptest.NewRecorder()
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusTeapot)
+	}))
+	defer svr.Close()
+
+	// Test reverse proxy
+	r := &Reverso{originURL: parseServerURL(svr.URL)}
+	r.ServeHTTP(rec, req)
+
+	res := rec.Result()
+
+	if res.StatusCode != http.StatusTeapot {
+		t.Errorf("Expected status code '%v', got '%v'", http.StatusTeapot, res.StatusCode)
+	}
+}
+
+// Checks for internal server errors when using an invalid server URL
+func TestInvalidOriginURL(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	r := &Reverso{} // empty origin URL
+	r.ServeHTTP(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != http.StatusInternalServerError {
+		t.Errorf("Expected status code '%v', got '%v'", http.StatusInternalServerError, res.StatusCode)
 	}
 }
 
