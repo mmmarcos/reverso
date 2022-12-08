3c6a89f82556aa68d823ef0f72a43a5571f92be3
Marcos Medrano <marcosmedrano0@gmail.com>
2022-12-08 11:46:49 -0300

    Added test for missing response headers from origin

--- main_test.go
+++ main_test.go
@@ -3,41 +3,58 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 )
 
-func TestHandleRequest(t *testing.T) {
-	const expected string = "Hello from the other side"
+func TestHandleSimpleRequest(t *testing.T) {
+	const expectedBodyStr string = "Hello from the other side"
+	const customHeaderKey string = "X-Test-Header"
+	const customHeaderVal string = "Custom header from origin"
 
 	// Mock objects
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
 	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, expected)
+		w.Header().Set(customHeaderKey, customHeaderVal)
+		fmt.Fprint(w, expectedBodyStr)
 	}))
 	defer svr.Close()
 
-	// Parse origin server URL
-	fmt.Println(string(svr.URL))
-	serverURL, err := url.Parse(svr.URL)
-	if err != nil {
-		t.Error(err)
-	}
-
 	// Test reverse proxy
-	r := &Reverso{originURL: *serverURL}
+	r := &Reverso{originURL: parseServerURL(svr.URL)}
 	r.ServeHTTP(rec, req)
 
 	res := rec.Result()
 	defer res.Body.Close()
-	body, err := io.ReadAll(res.Body)
+
+	bodyStr := readAll(res.Body)
+	if bodyStr != expectedBodyStr {
+		t.Errorf("Expected '%v', got '%v'", expectedBodyStr, bodyStr)
+	}
+
+	if v := res.Header.Get(customHeaderKey); v != customHeaderVal {
+		t.Errorf("Expected header value '%v', got '%v'", customHeaderVal, v)
+	}
+}
+
+// Parses the given raw URL into a url.URL
+func parseServerURL(rawURL string) url.URL {
+	serverURL, err := url.Parse(rawURL)
 	if err != nil {
-		t.Error(err)
+		log.Fatal(err)
 	}
-	if string(body) != expected {
-		t.Errorf("Expected '%v', got '%v'", expected, string(body))
+	return *serverURL
+}
+
+// Read all from r and return content as string
+func readAll(r io.Reader) string {
+	body, err := io.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
 	}
+	return string(body)
 }
