fec3804ee6da997a5c0e25da421ec6420e440912
Marcos Medrano <marcosmedrano0@gmail.com>
2022-12-10 13:14:12 -0300

    Added utility functions to refactor code

---
+++ util.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+)
+
+// Writes response into response writer
+func WriteResponse(rw http.ResponseWriter, res *http.Response) {
+	// Write header
+	for key, values := range res.Header {
+		for _, value := range values {
+			log.Println(key, value)
+			rw.Header().Add(key, value)
+		}
+	}
+	rw.WriteHeader(res.StatusCode)
+
+	// Write body
+	io.Copy(rw, res.Body)
+}
+
+// Dumps a http.Response into a bytes.Buffer
+func DumpResponse(res *http.Response) (b bytes.Buffer) {
+	if err := res.Write(&b); err != nil {
+		log.Println("Error dumping response to buffer")
+	}
+	return
+}
+
+// Read response data into http.Response
+func ReadResponse(resData []byte, req *http.Request) *http.Response {
+	res, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(resData)), req)
+	if err != nil {
+		log.Println("Error reading response from buffer")
+	}
+	return res
+}
