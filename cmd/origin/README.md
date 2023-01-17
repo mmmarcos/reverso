c21cc49664df29acc5e6c600de628eddca2e9ac8
Marcos Medrano <marcosmedrano0@gmail.com>
2023-01-17 19:58:32 +0100

    Added support for chunked transfer encoding

--- cmd/origin/README.md
+++ cmd/origin/README.md
@@ -1,16 +1,38 @@
 # README
 
-This command starts a simple origin server listening on "localhost:8081" 
-for testing purposes.
+This command starts a simple "echo" server on `localhost:8081` for testing
+purposes.
 
-The request URL-encoded query string `s=<seconds>` can be used to control 
-the "Expires" header in the response. The server will add an "Expires" header
-with a date expiring after <seconds>. This is valid on any URL path.
+The query string of the request can be used to control some specific behavior:
 
-Example:
-```
-$ curl -i http://localhost:8081/hello\?s\=10
+- `expires=<N>`: will add the "Expires" header to the response, expiring
+  after `N` seconds.
+- `chunked=<N>`: will send `N` chunks of data (chunked transfer encoding) before
+  actual echo response.
+
+Examples:
+```console
+$ curl -i http://localhost:8081/hello
 HTTP/1.1 200 OK
+...
+
+/hello
+
+$ curl -i http://localhost:8081/hello\?expires\=3
+HTTP/1.1 200 OK
+...
 Expires: Sun, 11 Dec 2022 03:33:38 GMT
+
+/hello
+
+$ curl -i http://localhost:8081/hello\?chunked\=3
+HTTP/1.1 200 OK
+...
+Transfer-Encoding: chunked
+
+Chunk 1
+Chunk 2
+Chunk 3
+/hello
 ```
 
--- cmd/origin/main.go
+++ cmd/origin/main.go
@@ -10,23 +10,53 @@ import (
 )
 
 func main() {
-	const listenOn = ":8081"
+	log.SetFlags(log.Ltime | log.Lmicroseconds)
 	log.SetPrefix("[origin] ")
-	log.Printf("Listen on: '%v'", listenOn)
-	log.Fatal(http.ListenAndServe(listenOn, http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		log.Printf("Request received at: %s\n", time.Now())
 
-		// Add "Expires" header for queries containing "s=<seconds>"
-		if m, err := url.ParseQuery(req.URL.RawQuery); err == nil {
-			if seconds, err := strconv.Atoi(m.Get("s")); err == nil {
-				now := time.Now().In(time.FixedZone("GMT", 0))
-				expires := now.Add(time.Duration(seconds) * time.Second)
-				rw.Header().Set("Expires", expires.Format(http.TimeFormat))
-				log.Printf("Added Header Expires: %v", rw.Header().Get("Expires"))
+	const addr string = ":8081"
+	log.Printf("Listen address: '%s'", addr)
+	log.Fatal(http.ListenAndServe(addr, http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		log.Printf("Received request: '%s'\n", req.URL.Path)
+
+		// Parse query string
+		m, err := url.ParseQuery(req.URL.RawQuery)
+		if err != nil {
+			msg := fmt.Sprintf("Error parsing query string: '%s'", req.URL.RawQuery)
+			log.Println(msg)
+			rw.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(rw, msg)
+			return
+		}
+
+		// Add "Expires" header if query string contains "expires=<N>"
+		if m.Has("expires") {
+			now := time.Now().In(time.FixedZone("GMT", 0))
+			seconds, _ := strconv.Atoi(m.Get("expires")) // 0 seconds in case of error
+
+			// Expires N seconds from now
+			expires := now.Add(time.Duration(seconds) * time.Second).Format(http.TimeFormat)
+			rw.Header().Set("Expires", expires)
+			log.Printf("Expires: %v", expires)
+		}
+
+		// Send chunked data if query string contains "chunked=<N>"
+		if m.Has("chunked") {
+			if flusher, ok := rw.(http.Flusher); ok {
+				chunks, _ := strconv.Atoi(m.Get("chunked")) // 0 chunks in case of error
+				for i := 1; i <= chunks; i++ {
+					msg := fmt.Sprintf("Chunk %d", i)
+					fmt.Fprintln(rw, msg)
+					flusher.Flush()
+					log.Println(msg)
+					time.Sleep(1 * time.Second)
+				}
 			}
 		}
-		rw.Header().Set("X-Custom-Header", "Origin")
 
-		_, _ = fmt.Fprintf(rw, "Response from origin for path '%v'\n", req.URL.Path)
+		// Add some custom header
+		rw.Header().Set("Custom-Header", "Don't panic!")
+
+		// Response body
+		fmt.Fprintln(rw, req.URL.Path)
 	})))
 }
